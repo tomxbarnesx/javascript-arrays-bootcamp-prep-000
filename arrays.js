b687d414b735718bbef0049cd8f3607e65b94b3a
Thomas Barnes <barnes.thomas.c@gmail.com>
2018-05-22 17:51:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array;
 }
 
-function removeElementFromBeginningOfArray(array){
+function removeElementFromBeginningOfArray(array) {
   array.slice(1);
   return array;
 }
